63775f10961a0c8ecd635348bdce40c9b9a2752f
Mar-yam <bakaryibbaane@gmail.com>
2026-04-28 23:04:57 +0200

    feat: intégration de l'affichage dynamique avec les logos et stades de Supabase

---
+++ app_bdd.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+@app.route('/')
+def afficher_matchs_bdd():
+    try:
+        
+        response = supabase.table("matchs").select(
+            "id, date, heure, league_name, home_name, home_logo, away_name, away_logo, venue_name"
+        ).execute()
+        
+        matchs_bdd = response.data
+        
+    except Exception as e:
+        print(f"Erreur de connexion Supabase : {e}")
+        matchs_bdd = []
+
+    return render_template("affichage.html", matchs=matchs_bdd)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
