426b3420e94f1a7e272a6881831943b2a1fdfd82
Imane <imaneabedje.aminou@ecoles-epsi.net>
2026-05-03 06:47:41 +0200

    recuperation des cotes via web-scrapping

--- scrappingtest.py
+++ scrappingtest.py
@@ -3,13 +3,22 @@ import requests   #On importe la bibliothèque requests pour pouvoir faire des r
 from bs4 import BeautifulSoup  #On importe BeautifulSoup depuis la bibliothèque bs4  pour analyser le HTML.
 
 # On choisit le site qu'on veut scraper
-url = "https://meilleurssitesdeparissportifs.fr/football_FRA_D_FRE_generic.html#article-0"
+url = "https://www.betexplorer.com/football/france/ligue-1/"
 
 #on récupere le site 
-response = requests.get(url)
+#e "User-Agent" c'est très important — sans ça le site croit que c'est un robot
+# et refuse de répondre. En mettant un User-Agent on se fait passer pour
+# un vrai navigateur Chrome
+response = requests.get(url, headers={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+})
+
 
 # On analyse le HTML de la page
 soup = BeautifulSoup(response.text, 'html.parser')
+tableaux = soup.find_all("table")
 
 # On affiche juste le titre de la page dont on a mis l'url ( on le recupere direct depuis le code source de la page )
-print("Page récupérée :", soup.title.text)
+print( soup.title.text)
+if tableaux:
+    print(tableaux[0])
